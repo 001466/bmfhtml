400381149c6c7cb4174abcf626004e2a663547e2
Easy <240018840@qq.com>
2017-12-14 15:34:39 +0800

    里程碑1

--- WebContent/order/js/accountant.js
+++ WebContent/order/js/accountant.js
@@ -1,155 +1,54 @@
-function mone(thid, che, totalMoney) {
-
-    var mon = document.getElementById("mon");
-
-    var bazhe1 = document.getElementById("bazhe1");
-
-    var bazhe2 = document.getElementById("bazhe2");
-
-    var taoshu = document.getElementById("taoshu");
-
-    //var hdmon = document.getElementById("hdmon");
-    if (che == 0) {
-
-        mon.innerHTML = 0;
-
-        //hdmon.value = 0;
-        bazhe1.style.display = "none";
-
-        bazhe2.style.display = "none";
-
-        taoshu.style.display = "none"
-
-    } else if (che == 1) {
-
-        mon.innerHTML = totalMoney;
-
-        //hdmon.value = totalMoney;
-        bazhe1.style.display = "none";
-
-        bazhe2.style.display = "none";
-
-        taoshu.style.display = "none"
-
-    } else {
-
-        totalMoney = Number(totalMoney);
-
-        mon.innerHTML = totalMoney;
-
-        //taoshu.innerHTML = "һ";
-        //hdmon.value = totalMoney + "-1";
-        bazhe1.style.display = "inline";
-
-        bazhe2.style.display = "inline";
-
-        taoshu.style.display = "inline"
-
-    }
-
-}
-
-
  
 
 
 function allcp(thid) {
 	 
-
+		
 	 if (thid.checked == true) {
-		 thid.parentNode.className = "redboder";
+		 thid.parentNode.className = "redborder";
 	 }else{
 		 thid.parentNode.className = "";
 	 }
 	
-	 return ;
 	 
     vals = thid.value.split("|");
 
-    var che = 0;
 
     var totalMoney = 0;
-
-    var prdAttr = 0;
-
-    jncheck = 0;
-    
     for (var i = 0; i < $(".ck").length; i++) {
-
         if ($(".ck")[i].checked == true) {
-
-            prdAttr = $(".ck")[i].value.split("|");
-
-            totalMoney = Number(totalMoney) + Number(prdAttr[0]);
-
-            che++
-
+            totalMoney = Number(totalMoney) + Number($(".ck")[i].value.split("|")[0]);
         }
-
     }
-    
-    
+    $("#productPrices").val(totalMoney);
+    document.getElementById("productPricesLabel").innerHTML=totalMoney;
+
    
-    $("#Prices").val(totalMoney);
+    
 
     var selectedUl = document.getElementById("productSelected");
-
     var li = document.createElement("li");
-
-    var a = document.createElement("a");
-
-    li.appendChild(a);
-
-    
-
     if (thid.checked == true) {
-
-    	a.innerHTML = vals[1];
-
+    	li.innerHTML = vals[1];
     	selectedUl.appendChild(li);
-
-    }
-
-    else {
-
-        var as = selectedUl.getElementsByTagName("li");
-
-        thid.parentNode.className = "";
-
-        for (var i = 0; i < as.length; i++) {
-
-            var txt = as[i].innerText || as[i].textContent;
-
+    }else {
+        var ls = selectedUl.getElementsByTagName("li");
+        for (var i = 0; i < ls.length; i++) {
+            var txt = ls[i].innerText || ls[i].textContent;
             if (txt === vals[1]) {
-
-                as[i].parentNode.removeChild(as[i])
-
+            	selectedUl.removeChild(ls[i])
             }
-
         }
-
     }
 
-    var pd = "";
-    var i = 0;
-    $('#productSelected li a').each(function () {
-        if (i == 0) {
-            pd += $(this).html()
-        }
-        else if (i == 1) {
-            pd += $(this).html();
-        }
-        else {
-            pd += "+" + $(this).html();
-        }
-        i++;
+   
+    
+    
+    var pds = "";
+    $('#productSelected li ').each(function () {
+    	pds += $(this).html()+"|";
     })
+    $("#productId").val(pds.substring(0,pds.length-1));
     
-    $("#Products").val(pd);
-    
-
-
-
-    //mone(thid, che, totalMoney)
 
 }
